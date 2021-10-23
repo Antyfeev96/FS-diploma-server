7298435196893892d5da0646926f5420e50f3517
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-10-23 21:12:01 +0300

    CORS enabled

--- routes/halls.routes.js
+++ routes/halls.routes.js
@@ -1,5 +1,5 @@
 const {Router} = require('express');
-const { validationResult } = require('express-validator');
+const {validationResult} = require('express-validator');
 const Hall = require('../models/Hall');
 const router = Router();
 
@@ -7,30 +7,32 @@ const router = Router();
 router.get(
     '/halls',
     async (req, res) => {
-    try {
-        const errors = validationResult(req);
+        try {
+            const errors = validationResult(req);
 
-        if (!errors.isEmpty()) {
-            return res.status(400).json({
-                errors: errors.array(),
-                message: "Ошибка при попытке получения всех залов"
-            })
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: "Ошибка при попытке получения всех залов"
+                })
+            }
+
+            const halls = await Hall.find()
+
+            if (!halls) {
+                res.status(400).json({"message": "Список залов пуст"})
+            }
+
+            setTimeout(() => {
+                res.status(201).json({
+                    "message": "Список залов получен",
+                    "halls": JSON.stringify(halls)
+                })
+            }, 2000)
+        } catch (e) {
+            res.status(500).json({"message": "Что-то пошло не так, попробуйте ещё раз."})
         }
-
-        const halls = await Hall.find()
-
-        if (!halls) {
-            res.status(400).json({"message": "Список залов пуст"})
-        }
-
-        res.status(201).json({
-          "message": "Список залов получен",
-          "halls": JSON.stringify(halls)
-        })
-    } catch (e) {
-        res.status(500).json({"message": "Что-то пошло не так, попробуйте ещё раз."})
     }
-  }
 )
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const mongoose = require('mongoose')
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json({ extended: true }))
 
 app.use('/api/auth', require('./routes/auth.routes'))
