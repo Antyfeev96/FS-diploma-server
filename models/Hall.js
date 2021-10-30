4bdf52b7d24616c011e8ce4c366391d885ee635c
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-10-30 19:46:49 +0300

    Checked prop added to Hall model

--- models/Hall.js
+++ models/Hall.js
@@ -2,7 +2,8 @@ const { Schema, model } = require('mongoose');
 
 const schema = new Schema({
     name: { type: String, required: true, unique: true },
-    rows: { type: Array, required: true }
+    rows: { type: Array, required: true },
+    checked: { type: Boolean, required: true }
 });
 
 module.exports = model('Hall', schema)
