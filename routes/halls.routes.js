3ad75f4f11e933ba82e6d91902ed3ff9f61ad313
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-10-31 21:24:50 +0300

    Deleting halls added

--- routes/halls.routes.js
+++ routes/halls.routes.js
@@ -54,7 +54,27 @@ router.post(
 
             setTimeout(() => {
                 res.status(201).json({
-                    "message": "Список залов обновлён",
+                    "message": "Зал добавлен, список залов обновлён",
+                    "halls": JSON.stringify(halls)
+                })
+            }, 2000)
+        } catch (e) {
+            res.status(500).json({"message": "Что-то пошло не так, попробуйте ещё раз."})
+        }
+    }
+)
+
+router.delete(
+    '/halls/:id',
+    async (req, res) => {
+        try {
+            await Hall.findByIdAndDelete(req.params.id)
+
+            const halls = await Hall.find()
+
+            setTimeout(() => {
+                res.status(201).json({
+                    "message": "Зал удалён, список залов обновлён",
                     "halls": JSON.stringify(halls)
                 })
             }, 2000)
